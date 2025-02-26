4d1234b0e9e283dd51270751e787698dd9b6d9e5
Hardelz <135560555+Hardelzs@users.noreply.github.com>
2025-02-26 17:39:57 +0100

    Restructuring Navigationbar

--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -6,10 +6,10 @@ import { useNavigate } from "react-router-dom";
 const Navigation = ({ userData }) => {
 
   const [isOpen, setIsOpen] = useState(false);
-  const [isDropdownOpen, setDropdownOpen] = useState(false);
+  // const [isDropdownOpen, setDropdownOpen] = useState(false);
 
   const toggleSidebar = () => setIsOpen(!isOpen);
-  const toggleDropdown = () => setDropdownOpen(!isDropdownOpen);
+  // const toggleDropdown = () => setDropdownOpen(!isDropdownOpen);
 
   const navigate = useNavigate();
 
@@ -50,7 +50,7 @@ const Navigation = ({ userData }) => {
                 alt="User"
                 className="w-24 h-24 rounded-full mb-4"
               />
-              {userData ? (
+              {/* {userData ? (
                 <div className="mt-4">
                   <p className="text-lg">Username: {userData.username}</p>
                   <p className="text-lg">Name: {userData.name}</p>
@@ -59,42 +59,17 @@ const Navigation = ({ userData }) => {
                 </div>
               ) : (
                 <p>Loading user data...</p>
-              )}
+              )} */}
             </div>
 
             {/* Dropdown Button */}
-            <div className="mt-10 flex flex-col gap-5">
-              <button
-                aria-expanded={isDropdownOpen}
-                aria-controls="dropdown"
-                onClick={toggleDropdown}
-                className="text-white w-full hover:ring-2 active:ring-4 focus:outline-none  font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex gap-12 items-center"
-                type="button"
-              >
-                Option 1{" "}
-                <svg
-                  className="w-2.5 h-2.5 ml-3"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 10 6"
-                >
-                  <path
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M1 1l4 4 4-4"
-                  />
-                </svg>
-              </button>
+             <div className="mt-10 flex flex-col gap-5">
 
-              {/* Dropdown Menu */}
+
+
               <div
                 id="dropdown"
-                className={`z-10 ${
-                  isDropdownOpen ? "block" : "hidden"
-                } w-full divide-y divide-gray-100 rounded-lg shadow-sm`}
+                className={`w-full divide-y divide-gray-100 rounded-lg shadow-sm`}
               >
                 <ul
                   className="py-2 text-sm text-gray-700 dark:text-gray-200"
@@ -137,28 +112,9 @@ const Navigation = ({ userData }) => {
                 </ul>
               </div>
 
-              {/* Additional Option */}
-              <button className="text-white w-full hover:ring-2 active:ring-4 focus:outline-none  font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex gap-12 items-center">
-                Option 2{" "}
-                <svg
-                  className="w-2.5 h-2.5 ml-3"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 10 6"
-                >
-                  <path
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M1 1l4 4 4-4"
-                  />
-                </svg>
-              </button>
             </div>
           </div>
-        </div>
+         </div>
       )}
     </>
   );
