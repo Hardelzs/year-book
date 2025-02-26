b80c1c5142434ea96c22e9623df1ac4ee2137633
Hardelz <135560555+Hardelzs@users.noreply.github.com>
2025-02-26 18:14:22 +0100

    Restructuring Navigationbar

--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -3,6 +3,9 @@ import { useState } from "react";
 import { CgProfile, CgClose } from "react-icons/cg";
 import { FaSignOutAlt } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
+import { GrDocumentText } from "react-icons/gr";
+import { CgWebsite } from "react-icons/cg";
+import { GoPeople } from "react-icons/go";
 
 const Navigation = ({ userData }) => {
   const [isOpen, setIsOpen] = useState(false);
@@ -23,7 +26,7 @@ const Navigation = ({ userData }) => {
 
       {/* Sidebar */}
       {isOpen && (
-        <div className="fixed top-0 right-0 h-full w-72 bg-[#161b22] text-white shadow-lg z-50 transition-all duration-200">
+        <div className="fixed top-0 right-0 h-full p-2 rounded-l-xl w-72 bg-[#464240] text-white shadow-lg z-50 transition-all duration-200">
           {/* Profile Section */}
           <div className="flex flex-col items-center p-4 border-b border-gray-600">
             <img
@@ -36,24 +39,49 @@ const Navigation = ({ userData }) => {
           </div>
 
           {/* Navigation Links */}
-          <ul className="py-2">
-            <li className="px-6 py-3 hover:bg-gray-700 cursor-pointer" onClick={() => navigate("/profile")}>
+          <ul className="py-2 rounded-sm border-b border-gray-500">
+            <li className="px-4 py-3 hover:bg-gray-700 cursor-pointer" onClick={() => navigate("/profile")}>
               Your Profile
             </li>
-            <li className="px-6 py-3 hover:bg-gray-700 cursor-pointer" onClick={() => navigate("/repositories")}>
+            <li className="px-4 py-3 hover:bg-gray-700 cursor-pointer" onClick={() => navigate("/repositories")}>
               Your Repositories
             </li>
-            <li className="px-6 py-3 hover:bg-gray-700 cursor-pointer" onClick={() => navigate("/projects")}>
+            <li className="px-4 py-3 hover:bg-gray-700 cursor-pointer" onClick={() => navigate("/projects")}>
               Your Projects
             </li>
-            <li className="px-6 py-3 hover:bg-gray-700 cursor-pointer" onClick={() => navigate("/settings")}>
+            <li className="px-4 py-3 hover:bg-gray-700 cursor-pointer" onClick={() => navigate("/settings")}>
               Settings
             </li>
           </ul>
 
+
+          {/* Documentation  */}
+          <div className="border-b border-gray-500 py-3">
+          <div
+            className="px-6 py-3 mt-auto hover:bg-gray-700 flex items-center cursor-pointer"
+          >
+            <CgWebsite className="mr-2" />
+            YearBook Website
+          </div>
+          <div
+            className="px-6 py-3 mt-auto hover:bg-gray-700 flex items-center cursor-pointer"
+          >
+            <GrDocumentText className="mr-2" />
+            YearBook Docs
+          </div>
+          <div
+            className="px-6 py-3 mt-auto hover:bg-gray-700 flex items-center cursor-pointer"
+            onClick={() => navigate("buymeacoffee.com/adewaleezee")}
+          >
+            <GoPeople className="mr-2" />
+            YearBook Support
+          </div>
+          </div>
+
+
           {/* Sign Out Button */}
           <div
-            className="px-6 py-3 mt-auto hover:bg-red-600 flex items-center cursor-pointer"
+            className="px-6 py-3 mt-auto hover:bg-red-500 flex items-center cursor-pointer"
             onClick={() => navigate("/")}
           >
             <FaSignOutAlt className="mr-2" />
