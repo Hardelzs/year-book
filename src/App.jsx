fe7099dd3bc11de3611746e391ebef48670e9270
Hardelz <135560555+Hardelzs@users.noreply.github.com>
2025-04-16 23:52:08 +0100

    git

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,23 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import YearBook from "./Components/YearBook";
-import Year2 from "./Components/Year2";
-import Year1 from "./Components/Year1";
-import Profile from "./Components/Profile";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import YearBook from './Components/YearBook'
+import Year2 from './Components/Year2'
+import Year1 from './Components/Year1'
+import Profile from './Components/Profile'
 // import Login from "./Components/LoginForm";
-import LoginPage from "./Components/LoginPage";
-
+import LoginPage from './Components/LoginPage'
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LoginPage
-         />} />
+        <Route path="/" element={<LoginPage />} />
         <Route path="/YearBook" element={<YearBook />} />
-        <Route path="/entry" element={<Year2/>} />
-        <Route path="/Year1" element={<Year1/>} />
-        <Route path="/Profile" element={<Profile/>} />
-        {/* same add for others */}
+        <Route path="/entry" element={<Year2 />} />
+        <Route path="/Year1" element={<Year1 />} />
+        <Route path="/Profile" element={<Profile />} />
       </Routes>
     </Router>
-  );
+  )
 }
 
 export default App
